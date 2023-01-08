ede798981e0551db152e38a7a62f5878204481c1
Ernest <ernestl8502@gmail.com>
2023-01-07 19:46:10 -0800

    Added health bars

--- bot/duel_helpers/duel_manager.py
+++ bot/duel_helpers/duel_manager.py
@@ -11,22 +11,22 @@ class DuelManager:
         self.player1 = player1
         self.player2 = player2
 
-        player1_name = player1.nick if player1.nick else player1.name
-        player2_name = player2.nick if player2.nick else player2.name
-
-        #Display embed for when players are choosing moves
-        self.choices_embed = discord.Embed(colour = discord.Colour.greyple())
-        self.choices_embed.set_footer(text = f"{player1_name}, {player2_name} please pick your combination")
-        self.choices_embed.add_field(name = player1_name, value = "___", inline = False)
-        self.choices_embed.add_field(name = player2_name, value = "___", inline = False)
-        self.choices_embed.set_author(name=f"{player1_name} 🆚 {player2_name}")
-        
-        self.interaction = interaction
+        self.player1_name = player1.nick if player1.nick else player1.name
+        self.player2_name = player2.nick if player2.nick else player2.name
 
         self.players_info_dict = {
             player1 : {"health" : 100, "moves" : list()}, 
             player2 : {"health" : 100, "moves" : list()}
-            }
+        }
+
+        #Display embed for when players are choosing moves
+        self.choices_embed = discord.Embed(colour = discord.Colour.greyple(), description=self.make_health_str())
+        self.choices_embed.set_footer(text = f"{self.player1_name}, {self.player2_name} please pick your combination")
+        self.choices_embed.add_field(name = self.player1_name, value = "___")
+        self.choices_embed.add_field(name = self.player2_name, value = "___")
+        self.choices_embed.set_author(name=f"{self.player1_name} 🆚 {self.player2_name}")
+        
+        self.interaction = interaction
 
     async def run_game(self) -> None:
         action_view = ActionView(self.player1, self.player2, self.choices_embed, self.players_info_dict)
@@ -45,18 +45,30 @@ class DuelManager:
             self.players_info_dict[self.player1]["moves"] = move_dict[self.player1]
             self.players_info_dict[self.player2]["moves"] = move_dict[self.player2]
 
+            results_embed = discord.Embed(title = "Results:", description=self.make_health_str())
+            
             for x in range(3):
                 await self.do_compare()
-
-                
-            results_embed = discord.Embed()
-            await self.interaction.response.edit_message(embed=results_embed)
+                results_embed.description = self.make_health_str()
+                await self.interaction.edit_original_response(embed=results_embed)
+                await asyncio.sleep(3)
+            
+            await self.interaction.edit_original_response(embed=results_embed)
 
             
             #Refreshes action view and replace current embed with the choices_embed
-            await asyncio.sleep(5)  
+            await asyncio.sleep(5)
+
+            #Clears slected moves from last round
+            self.players_info_dict[self.player1]["moves"] = []
+            self.players_info_dict[self.player2]["moves"] = []
+            self.choices_embed.clear_fields()
+            self.choices_embed.add_field(name = self.player1_name, value = "___")
+            self.choices_embed.add_field(name = self.player2_name, value = "___")
+            self.choices_embed.description = self.make_health_str()
+
             action_view = ActionView(self.player1, self.player2, self.choices_embed, self.players_info_dict)
-            await self.interaction.response.edit_message(embed = self.choices_embed, view= action_view)
+            await self.interaction.edit_original_response(embed = self.choices_embed, view= action_view)
             #break
     
     async def do_compare(self):
@@ -73,5 +85,19 @@ class DuelManager:
         if p2_move.lose_against(p1_move):
             p1_move.execute(player_info_dict = self.players_info_dict, receiver = self.player2)
             return
-        
+    
+    def make_health_str(self):
+        player1_str = f"💙{self.make_health_bar(self.player1)} {self.player1_name}"
+        player2_str = f"❤️{self.make_health_bar(self.player2)} {self.player2_name}"
+        return player1_str + "\n" + player2_str
+            
+    def make_health_bar(self, user : discord.User) -> str:
+        health = self.players_info_dict[user]["health"]
 
+        max_health = 100
+        max_bars = 20
+
+        dash_convert = int(max_health/max_bars)
+        health_display = "=" * int(health/dash_convert)
+        #missing_health_display = "-" * (max_bars - int(health/dash_convert))
+        return "|" + health_display + f"| {health}"
--- bot/duel_helpers/duel_moves/basic_moves.py
+++ bot/duel_helpers/duel_moves/basic_moves.py
@@ -20,20 +20,21 @@ class LightAttack(Move):
         super().__init__("Light", LIGHTID, [BLOCKID])
     
     def execute(self, player_info_dict=None, receiver: discord.User = None, **_):
-        player_info_dict[receiver]["health"] -= max(0, player_info_dict[receiver]["health"] - random.randint(3, 7))
+        player_info_dict[receiver]["health"] = max(0, player_info_dict[receiver]["health"] - random.randint(3, 7))
         
 class HeavyAttack(Move):
     def __init__(self) -> None:
         super().__init__("Heavy", HEAVYID, [LIGHTID])
     
     def execute(self, player_info_dict=None, receiver = None, **_):
-        player_info_dict[receiver]["health"] -= max(0, player_info_dict[receiver]["health"] - random.randint(10, 15))
+        player_info_dict[receiver]["health"] = max(0, player_info_dict[receiver]["health"] - random.randint(10, 15))
 
 class Block(Move):
     def __init__(self) -> None:
         super().__init__("Block", BLOCKID, [HEAVYID])
     
     def execute(self, player_info_dict = None, receiver = None, **_):
-        player_info_dict[receiver]["moves"][1] = DoNothing()
+        if len(player_info_dict[receiver]["moves"]) >= 2:
+            player_info_dict[receiver]["moves"][1] = DoNothing()
         return
     
