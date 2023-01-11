5b2969418902269ba06a7db03c950f23d536f832
Ernest <ernestl8502@gmail.com>
2023-01-10 17:22:31 -0800

    changed log file to discord.log

--- bot/punchy.py
+++ bot/punchy.py
@@ -10,7 +10,7 @@ class Punchy(commands.AutoShardedBot):
         super().__init__(command_prefix=command_prefix, **kwargs)
 
     async def start(self, token : str):
-        logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO, filemode="w")
+        logging.basicConfig(filename='discord.log', encoding='utf-8', level=logging.INFO, filemode="w")
 
         for filename in os.listdir("../Punchy/bot/cogs/commands"):
             if not filename.endswith(".py"):
@@ -19,7 +19,7 @@ class Punchy(commands.AutoShardedBot):
                 await self.load_extension("bot.cogs.commands." + filename.replace(".py", ""))
                 logging.info("Loading in cog: " + filename.replace(".py", ""))
             except:
-                logging.exception("ERROR ERROR ERROR DOES NOT COMPUTE")
+                logging.exception("ERROR ERROR ERROR DOES NOT COMPUTEඞඞඞ")
 
         print("Punchy is up and ready to roll!")
         await super().start(token)
